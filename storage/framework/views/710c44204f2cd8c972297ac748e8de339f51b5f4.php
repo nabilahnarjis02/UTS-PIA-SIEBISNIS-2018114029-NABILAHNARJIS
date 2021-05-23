1d2d1854c1af12ff84c0ecb3b34685a9dc9ee5c0
nabilahnarjis02 <72370026+nabilahnarjis02@users.noreply.github.com>
2021-05-24 01:24:28 +0700

    uts

---
+++ storage/framework/views/710c44204f2cd8c972297ac748e8de339f51b5f4.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title><?php echo $__env->yieldContent('Data Mahasiswa UCIC'); ?></title>
+  </head>
+  <body>
+  <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
+        <a class="navbar-brand" >Data Mahasiswa Universitas CIC</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+            <a class="nav-link active" href="/mahasiswas"> Mahasiswa</a>
+            <a class="nav-link" href="/matakuliahs">Matakuliah</a>
+            <a class="nav-link" href="/absensis">Absensi</a>
+            <a class="nav-link" href="/kontrakmks">Kontrak Mata Kuliah</a>
+            <a class="nav-link" href="/jadwals">Jadwal</a>
+            <a class="nav-link" href="/semesters">Semester</a>
+            </div>
+        </div>
+        </nav>
+      
+
+  <?php echo $__env->yieldContent('content'); ?>
+
+
+
+
+  </div>
+
+  <!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html><?php /**PATH C:\Users\admin\Downloads\Quiz-PIL-Nabilah-Narjis\resources\views/template.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/bf7603e19303e1c1b247a7852982026b76625744.php
@@ -0,0 +1,59 @@
+
+ 
+<?php $__env->startSection('content'); ?>
+    <div class="row mt-5 mb-5">
+        <div class="col-lg-12 margin-tb">
+            <div class="float-left">
+                <h2>Mahasiswa</h2>
+            </div>
+            <div class="float-right">
+                
+            </div>
+        </div>
+    </div>
+ 
+    <?php if($message = Session::get('success')): ?>
+    <div class="alert alert-success">
+        <p><?php echo e($message); ?></p>
+    </div>
+    <?php endif; ?>
+ 
+    <table class="table table-bordered">
+        <tr>
+        
+            <th width="20px" class="text-center">Id</th>
+            <th>Nama Mahasiswa</th>
+            <th>Alamat</th>
+            <th>No Telepon</th>
+            <th>Email</th>
+            <th width="280px"class="text-center">Action</th>
+        </tr>
+        <?php $__currentLoopData = $mahasiswas; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+        <tr>
+            <td class="text-center"><?php echo e(++$i); ?></td>
+            <td><?php echo e($post->nama_mahasiswa); ?></td>
+            <td><?php echo e($post->alamat); ?></td>
+            <td><?php echo e($post->no_tlp); ?></td>
+            <td><?php echo e($post->email); ?></td>
+            <td class="text-center">
+                
+ 
+                    
+ 
+                    
+ 
+                    <?php echo csrf_field(); ?>
+                    <?php echo method_field('DELETE'); ?>
+ 
+                    <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')">Delete</button>
+                </form>
+            </td>
+        </tr>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    </table>
+ 
+    <?php echo $mahasiswas->links(); ?>
+
+ 
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\admin\Downloads\Quiz-PIL-Nabilah-Narjis\resources\views/mahasiswas/index.blade.php ENDPATH**/ ?>
